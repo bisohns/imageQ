575c9e9a334c9794def14a16ec169f151cce924a
Deven96 <diretnandomnan@gmail.com>
2018-12-27 03:01:18 +0100

    fix code smells

--- ImageQ/settings/common.py
+++ ImageQ/settings/common.py
@@ -74,15 +74,6 @@ WSGI_APPLICATION = 'ImageQ.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
