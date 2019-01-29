bdc39ae80f9a0bfea457b3dd7c3d4ffe0a71494f
Deven96 <diretnandomnan@gmail.com>
2019-01-29 07:23:49 +0100

    removed sendgrid backend details

--- ImageQ/settings/production.py
+++ ImageQ/settings/production.py
@@ -6,11 +6,11 @@ DEBUG = False
 
 #email backend and vars
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = "sgbackend.SendgridBackend"
-EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = "support@ImageQ"
-SENDGRID_API_KEY = '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'
-SENDGRID_SANDBOX_MODE_IN_DEBUG = False
-SENDGRID_ECHO_TO_STDOUT = True
+# EMAIL_BACKEND = "sgbackend.SendgridBackend"
+# EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = "support@ImageQ"
+# SENDGRID_API_KEY = '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'
+# SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+# SENDGRID_ECHO_TO_STDOUT = True
 
 #heroku database
 DATABASES = {
